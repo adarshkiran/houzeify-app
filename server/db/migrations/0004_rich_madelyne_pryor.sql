3d1996798dd1c726bbabcf9458ac4d8b81754bdb
adarshkiran <142662528+adarshkiran@users.noreply.github.com>
2026-09-20 23:29:30 +0530

    Delete server directory

--- server/db/migrations/0004_rich_madelyne_pryor.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "projects" ADD COLUMN "organization_id" text;--> statement-breakpoint
-ALTER TABLE "projects" ADD COLUMN "status" text;--> statement-breakpoint
-ALTER TABLE "projects" ADD COLUMN "timeline_start" text;--> statement-breakpoint
-ALTER TABLE "projects" ADD COLUMN "timeline_completion" text;--> statement-breakpoint
-ALTER TABLE "projects" ADD CONSTRAINT "projects_organization_id_organizations_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organizations"("id") ON DELETE set null ON UPDATE no action;--> statement-breakpoint
-CREATE INDEX "projects_organization_id_idx" ON "projects" USING btree ("organization_id");--> statement-breakpoint
-CREATE INDEX "projects_organization_id_updated_at_idx" ON "projects" USING btree ("organization_id","updated_at");
